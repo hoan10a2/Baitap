4e44734f65009de10e5dd88e217f27c71e411f7c
user <user@Users-Computer-4.local>
2016-10-25 14:18:04 +0700

    update bai tap

--- bai1.php
+++ bai1.php
@@ -12,7 +12,28 @@ if (!$number){
  
 // Lặp từ 1 tới number để in ra màn hình
 for ($i = 1; $i <= $number; $i++){
-    echo '<li>Số: '.$i.'</li>';
+    echo '<li>Số: '.themdauso($i).'</li>';
+
 }
+ function themdauso($number) {
+
+    //Hàm thêm dấu chấm (.) vào một chuỗi số hàng ngàn
  
+        $len = strlen($number);
+        $counter = 3;
+        $result = "";
+        while ($len - $counter >= 0)
+        {
+            $con = substr($number, $len - $counter , 3);
+            $result = '.'.$con.$result;
+            $counter+= 3;
+        }
+        $con = substr($number, 0 , 3 - ($counter - $len) );
+        $result = $con.$result;
+        if(substr($result,0,1)=='.'){
+            $result=substr($result,1,$len+1);   
+        }
+        return $result;
+}
+
 ?>
